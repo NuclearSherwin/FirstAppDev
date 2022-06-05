6a137eeb2c9381eac843836d901dc8dece1e0cc0
Phong Mai <phongspacenasa@gmail.com>
2022-06-05 12:58:47 +0700

    edit register for roles

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,31 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using AppdevPhong.Models;
+using AppDevPhong.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
 namespace AppdevKhanhPhong.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
+    [Area(SD.Authenticated_Area)]
+    [Authorize(Roles=SD.Role_Admin +","+ SD.Role_Staff)]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
@@ -60,10 +67,22 @@ namespace AppdevKhanhPhong.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            
+            
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string PhoneNumber { get; set; }
+            
+            //drop down
+            [Required]
+            public string Role { get; set; }
+            public IEnumerable<SelectListItem> RoleList { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            GetRole();
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
@@ -74,22 +93,73 @@ namespace AppdevKhanhPhong.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
-                var result = await _userManager.CreateAsync(user, Input.Password);
+                ApplicationUser applicationUser = new ApplicationUser();
+                Trainee trainee = new Trainee();
+                Trainer trainer = new Trainer();
+                IdentityResult result = new IdentityResult();
+                if (Input.Role == SD.Role_Trainee)
+                {
+                    trainee = new Trainee()
+                    {
+                        UserName = Input.Email, 
+                        Email = Input.Email,
+                        PhoneNumber = Input.PhoneNumber,
+                        Role = Input.Role,
+                        Name = Input.Name
+                    };
+                    result = await _userManager.CreateAsync(trainee, Input.Password);
+                    
+                }else if (Input.Role == SD.Role_Trainer)
+                {
+                    trainer = new Trainer()
+                    {
+                        UserName = Input.Email, 
+                        Email = Input.Email,
+                        PhoneNumber = Input.PhoneNumber,
+                        Role = Input.Role,
+                        Name = Input.Name
+                    };
+                    result = await _userManager.CreateAsync(trainer, Input.Password);
+                }
+                else
+                {
+                    applicationUser = new ApplicationUser()
+                    {
+                        UserName = Input.Email, 
+                        Email = Input.Email,
+                        PhoneNumber = Input.PhoneNumber,
+                        Role = Input.Role,
+                        Name = Input.Name
+                    };
+                    result = await _userManager.CreateAsync(applicationUser, Input.Password);
+                }
+                
                 if (result.Succeeded)
                 {
+                    if (Input.Role == SD.Role_Trainee)
+                    {
+                        await _userManager.AddToRoleAsync(trainee, trainee.Role);
+                        
+                    }else if (Input.Role == SD.Role_Trainer)
+                    {
+                        await _userManager.AddToRoleAsync(trainer, trainer.Role);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(applicationUser, applicationUser.Role);
+                    }
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    // var callbackUrl = Url.Page(
+                    //     "/Account/ConfirmEmail",
+                    //     pageHandler: null,
+                    //     values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                    //     protocol: Request.Scheme);
+                    //
+                    // await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -97,8 +167,9 @@ namespace AppdevKhanhPhong.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        // await _signInManager.SignInAsync(user, isPersistent: false);
+                        // return LocalRedirect(returnUrl);
+                        return RedirectToAction("Index", "Users", new {Area = "Authenticated"});
                     }
                 }
                 foreach (var error in result.Errors)
@@ -107,8 +178,32 @@ namespace AppdevKhanhPhong.Areas.Identity.Pages.Account
                 }
             }
 
+            GetRole();
             // If we got this far, something failed, redisplay form
             return Page();
         }
+        private void GetRole()
+        {
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Where(u=> u.Name != SD.Role_Trainee).Select(x=> x.Name).Select(i=> new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
+            if (User.IsInRole(SD.Role_Staff))
+            {
+                Input = new InputModel()
+                {
+                    RoleList = _roleManager.Roles.Where(u=> u.Name == SD.Role_Trainee).Select(x=> x.Name).Select(i=> new SelectListItem
+                    {
+                        Text = i,
+                        Value = i
+                    })
+                };
+            }
+        }
+        
     }
 }
